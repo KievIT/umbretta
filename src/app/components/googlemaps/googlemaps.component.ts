a36e98caf8717d79002220f21cda0c7a91b22efe
talavas <o.talavas@gmail.com>
2017-11-10 22:34:22 +0200

    Responsive layouts.

--- src/app/components/googlemaps/googlemaps.component.ts
+++ src/app/components/googlemaps/googlemaps.component.ts
@@ -7,43 +7,7 @@ import { Component } from '@angular/core';
       height: 550px;
     }
   `],
-  template: `
-  <div class="row">
-    <div class="col-3"></div>
-      <div class="col-6 LayoutText" style="height: 100px; margin: 20px 0 -160px 0; z-index: 100; background-color: rgba(255, 255, 255, 0,5);">
-        <div class="row">
-          <div class="col">
-            <h2 class="text-center">Umbretta Kiosks on the Map</h2>
-          </div>
-        </div>
-        <div class="row" style="height: 58%">
-          <div class="col btn-cities text-center align-self-end" (click)="onClick(40.705869, -74.009936)"> <h6>New York</h6> </div>
-          <div class="col btn-cities text-center align-self-end" (click)="onClick(42.358852, -71.056855)"> <h6>Boston</h6> </div>
-          <div class="col btn-cities text-center align-self-end" (click)="onClick(41.853854, -87.633130)"> <h6>Chicago</h6> </div>
-          <div class="col btn-cities text-center align-self-end" (click)="onClick(34.049750, -118.33762)"> <h6>Los Angeles</h6> </div>
-          <div class="col btn-cities text-center align-self-end" (click)="onClick(37.771725, -122.419750)"> <h6>San Francisco</h6> </div>
-        </div>
-      </div>
-    <div class="col-3"></div>
-  </div>
-  <div class="row">
-    <div class="col">
-        <agm-map [latitude]="lat" [longitude]="lng" [zoom]="zoom" [mapTypeId]="'roadmap'">
-          <agm-marker [latitude]="lat" [longitude]="lng"></agm-marker>
-          <agm-marker *ngFor="let m of markers; let i = index"
-              (markerClick)="clickedMarker(m.label, i)"
-              [latitude]="m.lat"
-              [longitude]="m.lng"
-              [label]="m.label"
-              [markerDraggable]="m.draggable">
-              <agm-info-window [disableAutoPan]="true">
-               {{m.info}}
-            </agm-info-window>
-          </agm-marker>
-        </agm-map>
-      </div>
-    </div>
-  `,
+  templateUrl: './googlemaps.component.html',
 })
 // <agm-map [latitude]="lat" [longitude]="lng" [zoom]="zoom" [mapTypeId]="'hybrid'"></agm-map>
 // worked for me... Options are: 'roadmap' | 'hybrid' | 'satellite' | 'terrain'
